d1e9e5cb2dc2d978afd923d3096517b3c9eb9654
OliverZulett <oliverzulett@gmail.com>
2020-05-09 16:46:56 -0400

    Editado por: SUDO en: 09/05/2020 a horas: 16:46

--- dist/config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    DB: {
-        LOCAL_DB: 'mongodb://admin:admin@localhost:27017/bakeryStoreDB',
-        ATLAS_DB: ''
-    }
-};
